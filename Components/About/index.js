e0eec17df581f198e1f5b74f3ab55ba345048f71
Shikhar <ishgupta2015@gmail.com>
2022-07-04 18:17:17 +0530

    added

--- Components/About/index.js
+++ Components/About/index.js
@@ -54,7 +54,7 @@ const About = () => {
             anymore. Now I can develop my own digital creations into an online
             experience, while having full control of design, motion and build.
           </Typography>
-          <Typography variant="h2" className={AboutStyles.Rtitle} fontSize={fontSizes}>
+          <Typography variant="h2" className={AboutStyles.Rtitle} fontSize={FontSizes.subHeading}>
             FIND MY <span className="important-word">RESUME</span> HERE
           </Typography>
           <StyledButton>Download Resume</StyledButton>
